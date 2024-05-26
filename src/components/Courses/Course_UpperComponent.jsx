b64d16d2b54dd76d61ef776177294b3302591b82
DNhanSuiton810 <136873531+DNhanSuiton810@users.noreply.github.com>
2024-05-26 21:06:22 +0700

    hihi

--- src/components/Courses/Course_UpperComponent.jsx
+++ src/components/Courses/Course_UpperComponent.jsx
@@ -1,16 +1,23 @@
-import "bootstrap/dist/css/bootstrap.css";
-import "./Courses.css";
+import 'bootstrap/dist/css/bootstrap.css'
+import './Courses.css'
+import AwesomeSlider from 'react-awesome-slider'
+import 'react-awesome-slider/dist/styles.css'
 
-export default function Course_Advertising({
-  title,
-  description,
-  buttonFunctionName,
-}) {
+const slider = (
+  <AwesomeSlider>
+    <div>{Course_Advertising}</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+  </AwesomeSlider>
+)
+
+export default function Course_Advertising({ title, description, buttonFunctionName }) {
   return (
     <div id="Course_Advertising_Sliding">
       <h3>{title}</h3>
       <p>{description}</p>
       <button type="button">{buttonFunctionName}</button>
     </div>
-  );
+  )
 }
--- src/components/Courses/Courses_FullPage.jsx
+++ src/components/Courses/Courses_FullPage.jsx
@@ -1,38 +1,40 @@
-import Course_Advertising from "./Course_UpperComponent.jsx";
-import { Courses_UpperComponentData } from "../../data/Courses_data.js";
-import { useState } from "react";
-// import AwesomeSlider from "react-awesome-slider";
-// import "react-awesome-slider/dist/styles.css";
-import Course_SubAdvertising from "./Course_LowerComponent.jsx";
+import Course_Advertising from './Course_UpperComponent.jsx'
+import { Courses_UpperComponentData } from '../../data/Courses_data.js'
+import { useState } from 'react'
+import AwesomeSlider from 'react-awesome-slider'
+import 'react-awesome-slider/dist/styles.css'
+import Course_SubAdvertising from './Course_LowerComponent.jsx'
 
-// const slider = (
-//   <AwesomeSlider>
-//     <div>
-//       {" "}
-//       <Course_Advertising
-//         title={Courses_UpperComponentData[0].title}
-//         description={Courses_UpperComponentData[0].description}
-//         buttonFunctionName={Courses_UpperComponentData[0].buttonFunctionName}
-//       />
-//     </div>
-//     <div>
-//       {" "}
-//       <Course_Advertising
-//         title={Courses_UpperComponentData[1].title}
-//         description={Courses_UpperComponentData[1].description}
-//         buttonFunctionName={Courses_UpperComponentData[1].buttonFunctionName}
-//       />
-//     </div>
-//     <div>
-//       {" "}
-//       <Course_Advertising
-//         title={Courses_UpperComponentData[2].title}
-//         description={Courses_UpperComponentData[2].description}
-//         buttonFunctionName={Courses_UpperComponentData[2].buttonFunctionName}
-//       />
-//     </div>
-//   </AwesomeSlider>
-// );
+function slider() {
+  return (
+    <AwesomeSlider>
+      <div>
+        {' '}
+        <Course_Advertising
+          title={Courses_UpperComponentData[0].title}
+          description={Courses_UpperComponentData[0].description}
+          buttonFunctionName={Courses_UpperComponentData[0].buttonFunctionName}
+        />
+      </div>
+      <div>
+        {' '}
+        <Course_Advertising
+          title={Courses_UpperComponentData[1].title}
+          description={Courses_UpperComponentData[1].description}
+          buttonFunctionName={Courses_UpperComponentData[1].buttonFunctionName}
+        />
+      </div>
+      <div>
+        {' '}
+        <Course_Advertising
+          title={Courses_UpperComponentData[2].title}
+          description={Courses_UpperComponentData[2].description}
+          buttonFunctionName={Courses_UpperComponentData[2].buttonFunctionName}
+        />
+      </div>
+    </AwesomeSlider>
+  )
+}
 
 export default function Courses_FullPage() {
   return (
@@ -45,6 +47,7 @@ export default function Courses_FullPage() {
     //       buttonFunctionName={course.buttonFunctionName}
     //     />
     //   ))}
+
     <div id="Courses_FullPage">
       <Course_Advertising
         title={Courses_UpperComponentData[0].title}
@@ -53,5 +56,5 @@ export default function Courses_FullPage() {
       />
       <Course_SubAdvertising />
     </div>
-  );
+  )
 }
